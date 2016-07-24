386f4ef4f1e75a50694d4c2ee550432d6ff87f1f
OpesMentis <planchot.antoine@gmail.com>
2016-07-25 00:20:03 +0200

    Ajout de «all_fill» et «init_pos_opt».

--- jeu.h
+++ jeu.h
@@ -3,7 +3,9 @@
 
 int printJeu(SDL_Surface *ecran);
 void chx_code(int code[4]);
-void init_pos_sec (SDL_Rect pos[4], int n);
+void init_pos_chx (SDL_Rect pos[4], int n);
 int is_over(int x, int y, SDL_Surface s, SDL_Rect p);
+int all_fill(int code[4]);
+void init_pos_opt (SDL_Rect pos[2], int n);
 
 #endif
